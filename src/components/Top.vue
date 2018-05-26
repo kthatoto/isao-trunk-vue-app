765adba293cfa30ad915213f5688a80daacb7780
kthatoto <kthatoto@gmail.com>
2018-05-26 23:47:51 +0900

    Clean initialized component

---
+++ src/components/Top.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="top">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
