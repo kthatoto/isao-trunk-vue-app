8a59d9b15a05640a6a5e06c82c1273e8656b4dd3
kthatoto <kthatoto@gmail.com>
2018-05-30 20:27:42 +0900

    Added header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <img src="@/assets/cat.jpg">
+    <img src="@/assets/cat.jpg" class="header__image">
   </div>
 </template>
 <script>
@@ -13,8 +13,19 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .header {
-  background-color: darkblue;
+  background-color: navy;
+  width: 100%;
+  height: 60px;
+  &__image {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px solid white;
+    float: right;
+    margin-top: 5px;
+    margin-right: 5px;
+  }
 }
 </style>
