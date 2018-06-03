f987c07cdd9c0e2f01f04325753d50eb2953fca9
kthatoto <kthatoto@gmail.com>
2018-06-04 00:29:58 +0900

    Added skills list

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <div class="skills">
+    <h2>SKILLS</h2>
+    <ul class="skills__list">
+      <li v-for="skill in skills" class="skills__item">{{ skill }}</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      skills: [
+        'JavaScript', 'Vue.js'
+      ]
     }
   },
   methods: {
@@ -12,4 +20,26 @@ export default {
 }
 </script>
 <style scoped>
+.skills {
+  color: white;
+  background-color: navy;
+  padding: 50px 0;
+}
+.skills h2 {
+  text-align: center;
+}
+.skills__list {
+  width: 60%;
+  margin: 0 auto;
+  text-align: center;
+}
+.skills__item {
+  display: inline-block;
+  background-color: white;
+  color: navy;
+  padding: 5px 10px;
+  margin: 5px;
+  font-weight: bold;
+  border-radius: 3px;
+}
 </style>
