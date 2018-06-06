90178bba5b44e447ef1acd701842a091aef3fe10
kthatoto <kthatoto@gmail.com>
2018-06-06 18:20:03 +0900

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+.footer {
+  background-color: #333;
+  height: 100px;
+}
+</style>
